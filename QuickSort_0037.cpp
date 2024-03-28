5ba8a62fbde513c4e57cb0f1b24d369f10cce1d7
prdpta <pradiptaputramei01@gmail.com>
2024-03-28 18:11:25 +0700

    commit 17

--- QuickSort_0037.cpp
+++ QuickSort_0037.cpp
@@ -98,4 +98,11 @@ void display() {
     cout << "Sorted Array" << endl;
     cout << "------------------" << endl;
 
-    
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasions: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
