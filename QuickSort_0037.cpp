febe34e3ca34473da897b3a64150b1dfc513824e
prdpta <pradiptaputramei01@gmail.com>
2024-03-28 17:59:41 +0700

    commit 10

--- QuickSort_0037.cpp
+++ QuickSort_0037.cpp
@@ -54,4 +54,8 @@ void q_short(int low, int high)
     j = high; //step 4
 
     
-    
+    while (i <= j) //step 10
+    {
+        //search for an element greater than pivot
+        while ((arr[i] <= pivot) && (i <= high)) //step 5
+        
