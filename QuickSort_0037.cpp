1eca5818df40377491126939a1006e8a58bca9da
prdpta <pradiptaputramei01@gmail.com>
2024-03-28 18:15:56 +0700

    commit 20

--- QuickSort_0037.cpp
+++ QuickSort_0037.cpp
@@ -119,4 +119,10 @@ int main()
         if (ch == 'n' || ch == 'N')
             break;
         
-        
+        system("pause");
+        system("cls");
+
+    } while (true);
+
+    return 0;
+}
