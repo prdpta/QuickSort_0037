90228bab24b6d465c4545d3775879636a5024e57
prdpta <pradiptaputramei01@gmail.com>
2024-03-28 17:45:28 +0700

    commit 3

--- QuickSort_0037.cpp
+++ QuickSort_0037.cpp
@@ -7,3 +7,10 @@ int cmp_count = 0; // number of comparasion
 int mov_count = 0; // number of data movements
 int n;
 
+void input() {
+    while (true)
+    {
+        cout << "Masukan panjang elemen array: ";
+        cin >> n;
+
+        
